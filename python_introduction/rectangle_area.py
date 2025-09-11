060b63b0128a7c25f28fd5437d427cf17fa75e2a
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-09-11 12:52:38 +0000

    Python Introduction Folder

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+length = 10
+
+width = 5
+
+area = length * width
+
+=======
+length = 10
+
+width = 5
+
+area = length * width
+
+>>>>>>> 051a2594912c8ae505c889b1654a53444404afe9
+print(f"The area of the rectangle is: {area}")
